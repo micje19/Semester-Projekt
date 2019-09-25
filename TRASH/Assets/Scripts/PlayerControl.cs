678bdc332bee0319f5e1c829ab4f15f7ba380bdf
Mickas Jensen <micje19@student.sdu.dk>
2019-09-26 01:35:26 +0200

    Unity Project

---
+++ TRASH/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Vector3 pos = transform.position;
+            // Instantiate at player specific position and zero rotation.
+            GameObject SandTile;
+            SandTile = Instantiate(myPrefab, pos + new Vector3(-1.16f, 1.87f, 0), Quaternion.identity);
+            // Move our position a step closer to the target.
+            float step = 150f * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, SandTile.transform.position, step);
+            Debug.Log("Object was spawned, left of player position");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Vector3 pos = transform.position;
+            // Instantiate at player specific position and zero rotation.
+            GameObject SandTile;
+            SandTile = Instantiate(myPrefab, pos + new Vector3(1.16f, 1.87f, 0), Quaternion.identity);
+            // Move our position a step closer to the target.
+            float step = 150f * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, SandTile.transform.position, step);
+            Debug.Log("Object was spawned, right of player position");
+        }
+    }
+}
---
+++ TRASH/Assets/Scripts/VectorGrid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorGrid : MonoBehaviour {
+    [SerializeField]
+    private int rows = 10;
+    [SerializeField]
+    private int cols = 4;
+    [SerializeField]
+    private float tileSize = 2f;
+    public GameObject myPrefab;
+
+
+    void Start()
+    {
+        CreateGrid();
+    }
+
+
+    private void CreateGrid()
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                GameObject tile = (GameObject)Instantiate(myPrefab, transform);
+
+                float posX = col * tileSize;
+                float posY = row * -tileSize;
+
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+
+        float gridW = cols * tileSize;
+        float gridH = rows * tileSize;
+        transform.position = new Vector2(-gridW / 2 + tileSize / 2, gridH / 2 - tileSize / 2);
+    }
+}
